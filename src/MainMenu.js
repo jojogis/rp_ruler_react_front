f9988d1e2113b9614004e9f4ab1e31833f641df8
jojogis <jojogis.lesha@yandex.ru>
2021-02-14 00:10:05 +0300

    Отображение аватарок серверов. Добавлен диалог добавления сервера.

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -1,11 +1,12 @@
 import * as React from "react";
-import {Fab, List, ListItem,  withStyles} from "@material-ui/core";
+import {Avatar, Fab, List, ListItem, withStyles} from "@material-ui/core";
 import {AssignmentInd, Add, Settings, Explore, Forum} from "@material-ui/icons";
 
 import {Divider} from "@material-ui/core";
 import AppContext from "./AppContext.js";
 import ProfileDialog from "./ProfileDialog";
 import ServersDialog from "./ServersDialog";
+import AddServerDialog from "./AddServerDialog";
 
 
 
@@ -15,12 +16,10 @@ class MainMenu extends React.Component{
         super(props);
         this.state = {
             profileDialogOpen:false,
-            serversDialogOpen:false
+            serversDialogOpen:false,
+            addServerDialogOpen:false
         };
-        this.handleProfileDialogClose = this.handleProfileDialogClose.bind(this);
-        this.handleProfileDialogOpen = this.handleProfileDialogOpen.bind(this);
-        this.handleServersDialogClose = this.handleServersDialogClose.bind(this);
-        this.handleServersDialogOpen = this.handleServersDialogOpen.bind(this);
+
 
     }
     handleServerClick(id,name){
@@ -28,22 +27,11 @@ class MainMenu extends React.Component{
     }
 
 
-    handleProfileDialogClose(){
-        this.setState({profileDialogOpen:false})
-    }
-    handleProfileDialogOpen(){
-        this.setState({profileDialogOpen:true})
-    }
-    handleServersDialogClose(){
-        this.setState({serversDialogOpen:false})
-    }
-    handleServersDialogOpen(){
-        this.setState({serversDialogOpen:true})
-    }
+
 
     render() {
         const {classes} = this.props;
-        return (<div><List><ListItem><Fab onClick={this.handleProfileDialogOpen} aria-label="add">
+        return (<div><List><ListItem><Fab onClick={() => this.setState({profileDialogOpen:true})} aria-label="add">
             <Settings />
         </Fab></ListItem>
                 <ListItem><Fab onClick={this.props.onToChatClick} aria-label="add">
@@ -52,17 +40,18 @@ class MainMenu extends React.Component{
                 <Divider className={classes.divider}/>
             {this.props.servers.map((item)=>(
                 <ListItem key={item.id}>
-                    <Fab className={classes.serverElem}
-                         onClick={() => this.handleServerClick(item.id)}>
-                    {item.name.substr(0,2)}</Fab></ListItem>
+                    <Avatar className={classes.avatar} onClick={() => this.handleServerClick(item.id)} src={"https://rp-ruler.ru/upload/"+item.avatar}>
+                        <Fab className={classes.serverElem}>{item.name.substr(0,2)}</Fab>
+                    </Avatar></ListItem>
             ))}
-            <ListItem><Fab color="primary" className={classes.serverElem}>
+            <ListItem><Fab color="primary" className={classes.serverElem} onClick={() => this.setState({addServerDialogOpen:true})}>
                 <Add/></Fab></ListItem>
-                <ListItem><Fab color="primary" onClick={this.handleServersDialogOpen} className={classes.serverElem}>
+                <ListItem><Fab color="primary" onClick={() =>this.setState({serversDialogOpen:true})} className={classes.serverElem}>
                     <Explore/></Fab></ListItem>
 
-            </List><ProfileDialog open={this.state.profileDialogOpen} onClose={this.handleProfileDialogClose}/>
-                    <ServersDialog open={this.state.serversDialogOpen} onServerConnect={this.props.onServerConnect} onClose={this.handleServersDialogClose}/>
+            </List><ProfileDialog open={this.state.profileDialogOpen} onClose={() =>this.setState({profileDialogOpen:false})}/>
+                    <ServersDialog open={this.state.serversDialogOpen} onServerConnect={this.props.onServerConnect} onClose={() => this.setState({serversDialogOpen:false})}/>
+                    <AddServerDialog open={this.state.addServerDialogOpen} onClose={() => this.setState({addServerDialogOpen:false})}/>
         </div>
         );
     }
@@ -72,6 +61,12 @@ const styles = {
     divider: {
 
         padding:"1px"
+    },
+    avatar:{
+        width:"56px",
+        height:"56px",
+        "background-color":"#e0e0e0",
+        "cursor":"pointer"
     },
     serverElem:{
         "font-size":"20px"
