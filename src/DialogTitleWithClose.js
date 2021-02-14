3ee092261f1635be2b41dace85e216ceda5cbf62
jojogis <jojogis.lesha@yandex.ru>
2021-02-14 13:05:03 +0300

    Заголовок диалога с крестиком (теперь точно)

---
+++ src/DialogTitleWithClose.js
@@ -0,0 +1,31 @@
+import {DialogTitle, IconButton, Typography, withStyles} from "@material-ui/core";
+import {Close} from "@material-ui/icons";
+import * as React from "react";
+
+const styles = (theme) => ({
+    root: {
+        margin: 0,
+        padding: theme.spacing(2),
+    },
+    closeButton: {
+        position: 'absolute',
+        right: theme.spacing(1),
+        top: theme.spacing(1),
+        color: theme.palette.grey[500],
+    },
+});
+
+const DialogTitleWithClose = withStyles(styles)((props) => {
+    const { children, classes, onClose, ...other } = props;
+    return (
+        <DialogTitle disableTypography className={classes.root} {...other}>
+            <Typography variant="h6">{children}</Typography>
+            {onClose ? (
+                <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+                    <Close />
+                </IconButton>
+            ) : null}
+        </DialogTitle>
+    );
+});
+export default DialogTitleWithClose;
